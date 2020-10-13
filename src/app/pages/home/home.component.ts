4c076ff9c1a0538f51f38519bab37c8cf92061c5
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-10-13 16:34:18 -0500

    extraer datos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../../services/youtube.service';
+import { Video } from '../../models/youtube.models';
+import {Swal} from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    
+  videos: Video [] = []
 
-  constructor() { }
+  constructor(private youtubeService: YoutubeService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  
+    this.cargarVideos();
+
+  }
+
+  cargarVideos(){
+    this.youtubeService.getVideos()
+        .subscribe(resp => {
+
+          
+          this.videos.push( ...resp );
+          console.log(this.videos)
+          
+        });
+  }
+
+  mostrarVideo(video:Video){
+    Swal.fire({
+      html: `
+      <h4> ${video.title}  </h4>
+      <hr>
+      <iframe width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+              frameborder="0" 
+              allow="accelerometer; 
+              autoplay; 
+              clipboard-write; 
+              encrypted-media; 
+              gyroscope; 
+              picture-in-picture" 
+              allowfullscreen>
+        </iframe>
+      `
+    })
+    
   }
 
 }
